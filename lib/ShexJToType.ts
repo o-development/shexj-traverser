bc77fa16fdb7339e636517a8325d12e3770243a9
jaxoncreed <jaxoncreed@gmail.com>
2022-01-06 14:03:36 -0800

    Completed simple shape

--- lib/ShexJToType.ts
+++ lib/ShexJToType.ts
@@ -2,32 +2,76 @@ import ShexJTraverser from "./shexJTraverser";
 import kitchenSinkSchema from "shex-test/schemas/kitchenSink.json";
 import * as dom from "dts-dom";
 import { jsonld2graphobject } from "jsonld2graphobject";
-import { circular } from "./testShape";
+import { circular, simple } from "./testShape";
+import { Annotation } from "shexj";
+
+export function iriToName(iri: string): string {
+  try {
+    const url = new URL(iri);
+    if (url.hash) {
+      return url.hash;
+    } else {
+      const splitPathname = url.pathname.split("/");
+      return splitPathname[splitPathname.length - 1];
+    }
+  } catch (err) {
+    return iri;
+  }
+}
+
+export function toCamelCase(text: string) {
+  return text
+    .replace(/([-_ ]){1,}/g, " ")
+    .split(/[-_ ]/)
+    .reduce((cur, acc) => {
+      return cur + acc[0].toUpperCase() + acc.substring(1);
+    });
+}
+
+export function nameFromObject(obj: {
+  id?: string;
+  annotations?: Annotation[];
+}): string | undefined {
+  const labelAnnotationObject = obj.annotations?.find(
+    (annotation) =>
+      annotation.predicate === "http://www.w3.org/2000/01/rdf-schema#label"
+  )?.object;
+  if (labelAnnotationObject && typeof labelAnnotationObject === "string") {
+    return toCamelCase(iriToName(labelAnnotationObject));
+  } else if (
+    labelAnnotationObject &&
+    typeof labelAnnotationObject !== "string"
+  ) {
+    return toCamelCase(labelAnnotationObject.value);
+  } else if (obj.id) {
+    return toCamelCase(iriToName(obj.id));
+  }
+}
+
+export function commentFromAnnotations(
+  annotations?: Annotation[]
+): string | undefined {
+  const commentAnnotationObject = annotations?.find(
+    (annotation) =>
+      annotation.predicate === "http://www.w3.org/2000/01/rdf-schema#comment"
+  )?.object;
+  if (typeof commentAnnotationObject === "string") {
+    // It's an IRI
+    return commentAnnotationObject;
+  } else {
+    return commentAnnotationObject?.value;
+  }
+}
 
 export const ShexJTypeTransformer = ShexJTraverser.createTransformer<{
   Schema: {
     return: dom.NamespaceDeclaration;
   };
   Shape: {
-    return: dom.InterfaceDeclaration | dom.UnionType | dom.IntersectionType;
-  };
-  ShapeOr: {
-    return: dom.InterfaceDeclaration | dom.UnionType | dom.IntersectionType;
-  };
-  ShapeAnd: {
-    return: dom.InterfaceDeclaration | dom.UnionType | dom.IntersectionType;
-  };
-  ShapeNot: {
-    return: dom.InterfaceDeclaration | dom.UnionType | dom.IntersectionType;
-  };
-  ShapeExternal: {
-    return: dom.InterfaceDeclaration | dom.UnionType | dom.IntersectionType;
+    return: dom.InterfaceDeclaration;
   };
   EachOf: {
-    return: dom.InterfaceDeclaration | dom.IntersectionType;
-  };
-  OneOf: {
-    return: dom.UnionType;
+    return: dom.ObjectType | dom.InterfaceDeclaration;
   };
   TripleConstraint: {
     return: dom.PropertyDeclaration;
@@ -38,99 +82,144 @@ export const ShexJTypeTransformer = ShexJTraverser.createTransformer<{
 }>({
   Schema: {
     transformer: async (
-      schema,
+      _schema,
       transformedChildren
     ): Promise<dom.NamespaceDeclaration> => {
-      const namespace = dom.create.namespace("namespace");
-      console.log(transformedChildren.shapes);
-      transformedChildren.shapes.forEach((shape, index) => {
-        namespace.members.push(
-          dom.create.alias(`typePlaceholder${index}`, shape)
-        );
+      const namespace = dom.create.namespace("");
+      transformedChildren.shapes?.forEach((shape) => {
+        if (
+          typeof shape !== "string" &&
+          (shape as dom.InterfaceDeclaration).kind === "interface"
+        ) {
+          namespace.members.push(shape as dom.InterfaceDeclaration);
+        }
       });
       return namespace;
     },
   },
-
   Shape: {
     transformer: async (shape, transformedChildren) => {
-      console.log("In Shape Transformer");
-      if (transformedChildren.expression.kind === "property") {
-        const newInterface = dom.create.interface(
-          "yetAnotherAnotherPlaceholder"
+      const shapeName = nameFromObject(shape) || "Shape";
+      const newInterface = dom.create.interface(shapeName);
+      if (
+        typeof transformedChildren.expression !== "string" &&
+        (transformedChildren.expression as dom.ObjectType).kind === "object"
+      ) {
+        newInterface.members.push(
+          ...(transformedChildren.expression as dom.ObjectType).members
         );
-        newInterface.members.push(transformedChildren.expression);
-        return newInterface;
       }
-      return transformedChildren.expression;
+      return newInterface;
     },
   },
   EachOf: {
-    transformer: async (
-      eachOf,
-      transformedChildren
-    ): Promise<dom.InterfaceDeclaration | dom.IntersectionType> => {
-      const newInterface = dom.create.interface("anotherPlaceholder");
-      const nonPropertyExpressions: (dom.UnionType | dom.IntersectionType)[] =
-        [];
+    transformer: async (eachOf, transformedChildren) => {
+      const name = nameFromObject(eachOf);
+      const objectType = name
+        ? dom.create.interface(name)
+        : dom.create.objectType([]);
       transformedChildren.expressions.forEach((expression) => {
-        if (expression.kind === "property") {
-          newInterface.members.push(expression);
-        } else if (expression.kind === "interface") {
-          newInterface.members.push(...expression.members);
-        } else {
-          nonPropertyExpressions.push(expression);
+        if (
+          typeof expression !== "string" &&
+          (expression as dom.PropertyDeclaration).kind === "property"
+        ) {
+          objectType.members.push(expression as dom.PropertyDeclaration);
         }
       });
-      if (nonPropertyExpressions.length === 0) {
-        return newInterface;
-      }
-      return dom.create.intersection([newInterface, ...nonPropertyExpressions]);
-    },
-  },
-  OneOf: {
-    transformer: async (oneOf, transformedChildren): Promise<dom.UnionType> => {
-      const newUnion = dom.create.union([]);
-      transformedChildren.expressions.forEach((child) => {
-        let actualChild;
-        if (child.kind === "property") {
-          const newInterface = dom.create.interface("yetAnotherPlaceholder");
-          newInterface.members.push(child);
-          actualChild = newInterface;
-        } else {
-          actualChild = child;
-        }
-        newUnion.members.push(actualChild);
-      });
-      return newUnion;
+      return objectType;
     },
   },
   TripleConstraint: {
-    transformer: async (
-      tripleConstraint,
-      transformedChildren
-    ): Promise<dom.PropertyDeclaration> => {
-      return dom.create.property(
-        tripleConstraint.predicate,
-        transformedChildren.valueExpr
+    transformer: async (tripleConstraint, transformedChildren) => {
+      const propertyName =
+        nameFromObject(tripleConstraint) ||
+        toCamelCase(iriToName(tripleConstraint.predicate));
+      const isArray = tripleConstraint.max !== 1;
+      const isOptional = tripleConstraint.min === 0;
+      let type: dom.Type = dom.type.undefined;
+      if (transformedChildren.valueExpr) {
+        type = transformedChildren.valueExpr as dom.Type;
+      }
+
+      const propertyDeclaration = dom.create.property(
+        propertyName,
+        isArray ? dom.type.array(type) : type,
+        isOptional ? dom.DeclarationFlags.Optional : dom.DeclarationFlags.None
       );
+      propertyDeclaration.jsDocComment = commentFromAnnotations(
+        tripleConstraint.annotations
+      );
+      return propertyDeclaration;
     },
   },
   NodeConstraint: {
-    transformer: async (
-      nodeConstraint,
-      transformedChildren
-    ): Promise<dom.Type> => {
-      return dom.type.string;
+    transformer: async (nodeConstraint, _transformedChildren) => {
+      if (nodeConstraint.datatype) {
+        switch (nodeConstraint.datatype) {
+          case "http://www.w3.org/2001/XMLSchema#string":
+          case "http://www.w3.org/2001/XMLSchema#ENTITIES":
+          case "http://www.w3.org/2001/XMLSchema#ENTITY":
+          case "http://www.w3.org/2001/XMLSchema#ID":
+          case "http://www.w3.org/2001/XMLSchema#IDREF":
+          case "http://www.w3.org/2001/XMLSchema#IDREFS":
+          case "http://www.w3.org/2001/XMLSchema#language":
+          case "http://www.w3.org/2001/XMLSchema#Name":
+          case "http://www.w3.org/2001/XMLSchema#NCName":
+          case "http://www.w3.org/2001/XMLSchema#NMTOKEN":
+          case "http://www.w3.org/2001/XMLSchema#NMTOKENS":
+          case "http://www.w3.org/2001/XMLSchema#normalizedString":
+          case "http://www.w3.org/2001/XMLSchema#QName":
+          case "http://www.w3.org/2001/XMLSchema#token":
+            return dom.type.string;
+          case "http://www.w3.org/2001/XMLSchema#date":
+          case "http://www.w3.org/2001/XMLSchema#dateTime":
+          case "http://www.w3.org/2001/XMLSchema#duration":
+          case "http://www.w3.org/2001/XMLSchema#gDay":
+          case "http://www.w3.org/2001/XMLSchema#gMonth":
+          case "http://www.w3.org/2001/XMLSchema#gMonthDay":
+          case "http://www.w3.org/2001/XMLSchema#gYear":
+          case "http://www.w3.org/2001/XMLSchema#gYearMonth":
+          case "http://www.w3.org/2001/XMLSchema#time":
+            return dom.type.string;
+          case "http://www.w3.org/2001/XMLSchema#byte":
+          case "http://www.w3.org/2001/XMLSchema#decimal":
+          case "http://www.w3.org/2001/XMLSchema#int":
+          case "http://www.w3.org/2001/XMLSchema#integer":
+          case "http://www.w3.org/2001/XMLSchema#long":
+          case "http://www.w3.org/2001/XMLSchema#negativeInteger":
+          case "http://www.w3.org/2001/XMLSchema#nonNegativeInteger":
+          case "http://www.w3.org/2001/XMLSchema#nonPositiveInteger":
+          case "http://www.w3.org/2001/XMLSchema#positiveInteger":
+          case "http://www.w3.org/2001/XMLSchema#short":
+          case "http://www.w3.org/2001/XMLSchema#unsignedLong":
+          case "http://www.w3.org/2001/XMLSchema#unsignedInt":
+          case "http://www.w3.org/2001/XMLSchema#unsignedShort":
+          case "http://www.w3.org/2001/XMLSchema#unsignedByte":
+            return dom.type.number;
+          case "http://www.w3.org/2001/XMLSchema#boolean":
+            return dom.type.boolean;
+          case "http://www.w3.org/2001/XMLSchema#hexBinary":
+            return dom.type.string;
+          case "http://www.w3.org/2001/XMLSchema#anyURI":
+            return dom.type.string;
+          default:
+            return dom.type.string;
+        }
+      }
+      if (nodeConstraint.nodeKind) {
+        return dom.type.string;
+      }
+      return dom.type.undefined;
     },
   },
 });
 
 async function run() {
   const result = await ShexJTypeTransformer.transform(
-    await jsonld2graphobject({ "@id": "SCHEMA", ...circular }, "SCHEMA"),
+    await jsonld2graphobject({ "@id": "SCHEMA", ...simple }, "SCHEMA"),
     "Schema"
   );
+  result.name = "circular";
   console.log("Final Result");
   console.log(result);
   console.log(dom.emit(result));
